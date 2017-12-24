fef4bd224f08fe7ba000cf9960f53f5196fd2a23
Sphexator <kristoffer.tunlid@gmail.com>
2017-12-24 01:34:43 +0100

    Add project files.

---
+++ BumpBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BumpBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
