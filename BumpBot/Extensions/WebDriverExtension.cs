6bea136f8db800788435773e9791f2618a21b534
Sphexator <kristoffer.tunlid@gmail.com>
2018-08-16 00:32:33 +0200

    initial commit

---
+++ BumpBot/Extensions/WebDriverExtension.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium.Internal;
+
+namespace BumpBot.Extensions
+{
+    public static class WebDriverExtension
+    {
+        private const long Speed = 10000;
+
+        public static IWebDriver ChromeDriverSettings(this ChromeDriver driver)
+        {
+            //driver.NetworkConditions.DownloadThroughput = Speed;
+            //driver.NetworkConditions.UploadThroughput = Speed;
+            //driver.NetworkConditions.Latency = TimeSpan.FromMilliseconds(500);
+            //driver.NetworkConditions.IsOffline = false;
+
+            driver.Url = "http://discord.me/";
+            
+            return driver;
+        }
+
+        public static IWebDriver WebDriverSettings(this IWebDriver web)
+        {
+            web.Manage().Timeouts().PageLoad = TimeSpan.FromMinutes(1);
+            return web;
+        }
+
+        public static async Task<IWebDriver> Login(this IWebDriver web, IConfiguration config)
+        {
+            web.FindElement(By.XPath("/html/body/div/div[1]/div/div/div/div/form/div[1]/input")).SendKeys(config["username"]);
+            web.FindElement(By.XPath("/html/body/div/div[1]/div/div/div/div/form/div[2]/input")).SendKeys(config["password"]);
+            await Task.Delay(2000);
+
+            try
+            {
+                web.FindElement(By.XPath("/html/body/div/div[1]/div/div/div/div/form/div[4]/div")).Click();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                web.FindElement(By.XPath("/html/body/div/div[1]/div/div/div/div/form/div[4]/div/button")).Click();
+            }
+
+            return web;
+        }
+
+        private static bool CheckLogin(this ISearchContext driver)
+        {
+            var check = driver.FindElement(By.LinkText("sign in"));
+            return check != null;
+        }
+    }
+}
--- BumpBot/Program.cs
+++ BumpBot/Program.cs
@@ -1,12 +1,80 @@
 ﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using BumpBot.Entities;
+using BumpBot.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace BumpBot
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private TwoCaptchaClient _client;
+        private IConfiguration _config;
+        private IWebDriver _web;
+        private static readonly Regex BumpTimer = new Regex(@"([0-9])(h).*?([0-9])(m).*?([0-9])(s)",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private static void Main() => new Program().BumpBot().GetAwaiter().GetResult();
+
+        private async Task BumpBot()
         {
-            Console.WriteLine("Hello World!");
+            _config = BuildConfig();
+            _web = new ChromeDriver(@"Componements\");
+            _client = new TwoCaptchaClient(_config["apiKey"]);
+
+            _web.WebDriverSettings();
+
+            var services = ConfigureServices();
+
+            _web.Url = "https://discord.me/signin";
+
+            await _web.Login(_config);
+            while (true)
+            {
+                if (_web.Url != "https://discord.me/dashboard") _web.Url = "https://discord.me/dashboard";
+
+                var bump = _web.FindElement(By.XPath("/html/body/div/div[1]/div[4]/div[1]/div/span/span"));
+                await Task.Delay(200);
+                if (bump.Text != "Available Now!")
+                {
+                    Console.WriteLine("no bump available");
+                    continue;
+                }
+
+                _web.FindElement(By.XPath("/html/body/div/div[1]/div[4]/div[1]/div/div[4]/div[1]/span/a"))
+                    .Click();
+
+                var googleKey = _web.FindElement(By.Id("recaptcha")).GetAttribute("data-sitekey");
+                var result = await _client.SolveCaptcha(googleKey, _web.PageSource, "username:password@ip:port",
+                    ProxyType.Http);
+                if (result == null) continue;
+                _web.FindElement(By.Id("g-recaptcha-response")).SendKeys(result);
+                await Task.Delay(500);
+                _web.FindElement(By.XPath("//*[@id=\"servers\"]/div[3]/button")).Click();
+            }
+        }
+
+        private IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton(_config);
+            services.AddSingleton(_client);
+            services.AddSingleton(_web);
+
+            return services.BuildServiceProvider();
+        }
+
+        private static IConfiguration BuildConfig()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json")
+                .Build();
         }
     }
 }
