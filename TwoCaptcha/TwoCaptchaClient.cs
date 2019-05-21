39a9d0c3bd38a0c5ca569f5b6887591375310e2d
Sphexator <kristoffer.tunlid@gmail.com>
2019-05-21 18:07:30 +0200

    minor change to two captcha client

--- TwoCaptcha/TwoCaptchaClient.cs
+++ TwoCaptcha/TwoCaptchaClient.cs
@@ -1,16 +1,28 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Net.Http;
 
 namespace TwoCaptcha
 {
     public class TwoCaptchaClient
     {
-        private string _token { get; set; }
-        public TwoCaptchaClient(string token) => _token = token;
+        private readonly string _token;
+        private readonly HttpClient _client;
 
-        public async Task StartAsync()
+        public TwoCaptchaClient(string token)
+        {
+            _token = token;
+            _client = new HttpClient();
+        }
+
+        public TwoCaptchaClient(string token, HttpClient client)
+        {
+            _token = token;
+            _client = client;
+        }
+
+        public string SolveCaptcha(string token)
         {
 
+            return null;
         }
     }
 }
